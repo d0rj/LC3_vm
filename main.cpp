11879c2313bacdcb4fe9f6ec5602abec95549ec1
d0rj <hanaan@internet.ru>
2021-07-13 18:03:05 +0300

    Added support for command-line argument: now you can choose path to file with programm.

--- main.cpp
+++ main.cpp
@@ -28,9 +28,21 @@ std::vector<uint16_t> loadBinary(std::string filename)
 }
 
 
-int main()
+int main(int argc, char** argv)
 {
-	auto program = loadBinary("../programms/programm.raw");
+	if (argc < 2) {
+		std::cout << "Error: required path to programm to execute as first argument.\n";
+		return 1;
+	}
+
+	std::vector<uint16_t> program;
+	try {
+		program = loadBinary(argv[1]);
+	} 
+	catch(const std::ifstream::failure& e) {
+		std::cout << "Error: exception while opening file with path : \'" << argv[1] << "\'.\n";
+		return 1;
+	}
 
 	auto memory = std::make_shared<lc3::Memory>(program.data(), program.size());
 	auto io = std::make_shared<lc3::IODevice>();
